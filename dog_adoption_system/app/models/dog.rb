2803a352bd14f7343e61fe8692f2653d32c84ee1
Harman212005 <143750972+Harman212005@users.noreply.github.com>
2025-07-28 10:36:33 +0530

    Update dog.rb

--- dog_adoption_system/app/models/dog.rb
+++ dog_adoption_system/app/models/dog.rb
@@ -1,2 +1,8 @@
 class Dog < ApplicationRecord
+  has_many :adoptions, dependent: :destroy
+  
+  has_many :owners, through: :adoptions
+  
+  scope :available, -> { where(available_for_adoption: true) }
+  scope :adopted, -> { where(available_for_adoption: false) }
 end
