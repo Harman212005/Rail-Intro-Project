43565c33947ae0761527167b5f0da492dbc8e26e
Harman212005 <Hharmanpreetkaur@academic.rrc.ca>
2025-07-31 09:47:55 +0530

    Fix search form to prominently display breed filter dropdown for hierarchical search

--- dog_adoption_system/app/controllers/dogs_controller.rb
+++ dog_adoption_system/app/controllers/dogs_controller.rb
@@ -1,8 +1,26 @@
 class DogsController < ApplicationController
   def index
+    @all_breeds = Dog.distinct.pluck(:breed).sort
+    
     @dogs = Dog.all.order(:name)
-    @available_dogs = Dog.available.order(:name)
-    @adopted_dogs = Dog.adopted.order(:name)
+    
+    if params[:breed].present?
+      @dogs = @dogs.where(breed: params[:breed])
+      @breed_filter = params[:breed]
+    end
+    
+    if params[:search].present?
+      search_term = params[:search].strip
+      @dogs = @dogs.where(
+        "name LIKE ? OR breed LIKE ? OR description LIKE ?", 
+        "%#{search_term}%", "%#{search_term}%", "%#{search_term}%"
+      )
+      @search_performed = true
+      @search_term = search_term
+    end
+    
+    @available_dogs = @dogs.where(available_for_adoption: true).order(:name)
+    @adopted_dogs = @dogs.where(available_for_adoption: false).order(:name)
     @total_count = @dogs.count
     @available_count = @available_dogs.count
     @adopted_count = @adopted_dogs.count
