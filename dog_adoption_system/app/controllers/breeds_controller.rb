49ae9b095d7c3d275bb9c000c5e27d48585d9741
Harman212005 <Hharmanpreetkaur@academic.rrc.ca>
2025-07-31 09:36:10 +0530

    Fix breed search redirect issue - prevent redirect when search returns no results

--- dog_adoption_system/app/controllers/breeds_controller.rb
+++ dog_adoption_system/app/controllers/breeds_controller.rb
@@ -1,6 +1,5 @@
 class BreedsController < ApplicationController
   def index
-    # Get all unique breeds with counts - simplified approach
     breeds_list = Dog.distinct.pluck(:breed).sort
     
     @breeds_data = breeds_list.map do |breed|
@@ -18,14 +17,27 @@ class BreedsController < ApplicationController
 
   def show
     @breed_name = params[:breed_name]
-    @dogs = Dog.where(breed: @breed_name).order(:name)
-    @available_dogs = @dogs.where(available_for_adoption: true)
-    @adopted_dogs = @dogs.where(available_for_adoption: false)
-
-    if @dogs.empty?
+    @search_performed = params[:search].present?
+    @search_term = params[:search]&.strip
+    
+    all_dogs_in_breed = Dog.where(breed: @breed_name)
+    
+    if all_dogs_in_breed.empty?
       redirect_to breeds_path, alert: "Breed '#{@breed_name}' not found."
       return
     end
+    
+    @dogs = all_dogs_in_breed.order(:name)
+    
+    if @search_performed && @search_term.present?
+      @dogs = @dogs.where(
+        "name LIKE ? OR description LIKE ?", 
+        "%#{@search_term}%", "%#{@search_term}%"
+      )
+    end
+    
+    @available_dogs = @dogs.where(available_for_adoption: true)
+    @adopted_dogs = @dogs.where(available_for_adoption: false)
     
     @breed_stats = {
       total: @dogs.count,
